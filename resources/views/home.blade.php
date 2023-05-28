970efc6cc173e11aeb135a77c1c923d97f97f346
ricky septiaji <rickyseptiaji2k00@gmail.com>
2023-05-28 22:10:25 +0700

    update kedua

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,22 +6,30 @@
 @section('content')
     <h1>Sistem Informasi Manajemen Karyawan</h1>
     <h3>Admin Dashboard</h3>
-    <div class="row gap-2">
-        <div class="col shadow p-3 bg-body-tertiary rounded">
-            <img src="assets/MultipleUsers.png" alt="user" height="100" width="100">
-          Data Pegawai
-        </div>
-        <div class="col shadow p-3 bg-body-tertiary rounded">
-            <img src="assets/calendar.png" alt="user" height="100" width="100">
-          Absensi Hari Ini
-        </div>
-        <div class="col shadow p-3 bg-body-tertiary rounded">
-            <img src="assets/check.png" alt="user" height="100" width="100">
-          Izin
-        </div>
-        <div class="col shadow p-3 bg-body-tertiary rounded">
-            <img src="assets/check.png" alt="user" height="100" width="100">
-            Lembur
-          </div>
+    <div class="col gap-2 d-flex">
+      <div class="card w-25 p-3 text-center">
+        <a href="/karyawan" class="shadow p-3 bg-body-tertiary rounded link-underline link-underline-opacity-0">
+          <img src="assets/MultipleUsers.png" alt="user" height="100" width="100">
+          <p class="text-black">Data Pegawai</p>
+        </a>
+      </div>
+      <div class="card w-25 p-3 text-center">
+        <a href="" class="shadow p-3 bg-body-tertiary rounded link-underline link-underline-opacity-0">
+          <img src="assets/calendar.png" alt="calendar" height="100" width="100">
+          <p class="text-black">Kalender</p>
+        </a>
+      </div>
+      <div class="card w-25 p-3 text-center">
+        <a href="" class="shadow p-3 bg-body-tertiary rounded link-underline link-underline-opacity-0">
+          <img src="assets/check.png" alt="check" height="100" width="100">
+          <p class="text-black">Absen</p>
+        </a>
+      </div>
+      <div class="card w-25 p-3 text-center">
+        <a href="" class="shadow p-3 bg-body-tertiary rounded link-underline link-underline-opacity-0">
+          <img src="assets/check.png" alt="check" height="100" width="100">
+          <p class="text-black">Izin</p>
+        </a>
+      </div>
       </div>
 @endsection
